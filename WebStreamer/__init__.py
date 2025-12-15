21ab62e61ed0a59dc21d3f1f930e3157c50f8b2a
Hash Hackers <admin@hashhackers.com>
2025-12-15 06:38:19 +0000

    auto-commit for 79b577ce-da3d-4418-adfe-1a46de084352

--- WebStreamer/__init__.py
+++ WebStreamer/__init__.py
@@ -2,10 +2,20 @@
 # Coding : Jyothis Jayanth [@EverythingSuckz]
 from time import time
 from .vars import Var
-from WebStreamer.bot import StreamBot
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
+# Configure limited thread pool executor to prevent thread exhaustion
+# This applies to all asyncio operations including Pyrogram clients
+# Reduced from default (CPU count * 5) to prevent "can't start new thread" errors on Heroku
+limited_executor = ThreadPoolExecutor(max_workers=8, thread_name_prefix="bot_pool_")
+
+# Get or create event loop and set the custom executor
 bot_loop = asyncio.get_event_loop()
+bot_loop.set_default_executor(limited_executor)
+
+# Import StreamBot AFTER configuring the executor
+from WebStreamer.bot import StreamBot
 
 __version__ = 2.2
 StartTime = time()
